ebdf08735214aec790a90310c52afce7eb690e62
Turtlesoup <lukebrown4290@hotmail.com>
2015-05-17 17:45:38 +0000

    added SMTP deafult from address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,7 @@
 class UserMailer < ActionMailer::Base
+  
+  default from: 'lukebrown4290@hotmail.com'
+  
   def password_reset(user)
     @user = user
     mail :to => user.email, :subject => "Password Reset"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
-  config.action_mailer.default_url_options = { :host => "https://memowebapp-turtlesoup.c9.io" }
+  config.action_mailer.default_url_options = { :host => "https://afternoon-falls-1285.herokuapp.com" }
 end
