496ffc8a2bc17f0f0a6994741311f84aa47d285b
Turtlesoup <lukebrown4290@hotmail.com>
2015-05-17 16:09:16 +0000

    set up production mail host

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# set the rails environment
-ENV['RAILS_ENV'] ||= 'production'
-
 # Initialize the Rails application.
 Rails.application.initialize!
