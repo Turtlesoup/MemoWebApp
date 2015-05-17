74804c83ebda7c33d512c54bffd1e4a14d16540c
Turtlesoup <lukebrown4290@hotmail.com>
2015-05-17 18:03:36 +0000

    added port to mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => "https://afternoon-falls-1285.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "https://afternoon-falls-1285.herokuapp.com:25" }
 end
