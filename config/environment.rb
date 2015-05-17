c18e0424d969e20e9378c81bff059f210a13f3ca
Turtlesoup <lukebrown4290@hotmail.com>
2015-05-17 14:43:52 +0000

    updated ruby version in gemfile

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# set the rails environment
+ENV['RAILS_ENV'] ||= 'production'
+
 # Initialize the Rails application.
 Rails.application.initialize!
