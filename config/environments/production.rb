0ec9221b7bc8e823b0254988242c343e96f29cdf
Turtlesoup <lukebrown4290@hotmail.com>
2015-05-17 18:35:25 +0000

    mailer settings updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => "https://afternoon-falls-1285.herokuapp.com:25" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =
+  {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "afternoon-falls-1285.herokuapp",
+    :user_name            => "turtlesoupgames@gmail.com",
+    :password             => "turtlesoup",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+  
 end
