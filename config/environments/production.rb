862a9c18db471888ae45c07075de71b8fe3de76e
Turtlesoup <lukebrown4290@hotmail.com>
2016-05-12 20:59:31 +0000

    changed mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => "https://afternoon-falls-1285.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "https://zentodolist.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings =
   {
